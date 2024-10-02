efa46628f2826698d3da03cc768640d40d1e6a1a
Igor Sanko <ig.sanko@gmail.com>
2024-10-03 02:10:25 +0300

    path definig

--- js/utils/createLevelsArray.js
+++ js/utils/createLevelsArray.js
@@ -4,9 +4,9 @@ const getLevel = async ( filePath ) => {
 };
 
  export const createLevelsArray = async () => {
-    const levelJson1 = await getLevel('../levels/1.json');
-    const levelJson2 = await getLevel('../levels/2.json');
-    const levelJson3 = await getLevel('../levels/3.json');
+    const levelJson1 = await getLevel('./levels/1.json');
+    const levelJson2 = await getLevel('./levels/2.json');
+    const levelJson3 = await getLevel('./levels/3.json');
 
     return [levelJson1.words, levelJson2.words, levelJson3.words];
 };
